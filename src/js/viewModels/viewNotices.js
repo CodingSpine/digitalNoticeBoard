b97fc9a1b2c6e2c001bcb775a9968192da600c9c
Ray Mathew <ray10mathew@gmail.com>
2018-08-29 17:49:46 +0530

    started film strip

--- src/js/viewModels/viewNotices.js
+++ src/js/viewModels/viewNotices.js
@@ -1,8 +1,17 @@
-define(['ojs/ojcore', 'knockout', 'globalContext', 'ojs/ojinputtext', 'ojs/ojbutton'],
-    function (oj, ko, context) {
+define(['ojs/ojcore', 'knockout', 'globalContext', 'ojs/ojinputtext', 'ojs/ojbutton', 'ojs/ojfilmstrip', 'ojs/ojpagingcontrol'],
+    function(oj, ko, context) {
         function viewNoticesViewModel() {
             var self = this;
-            
+            self.pagingModel = ko.observable(null);
+            this.handleBindingsApplied = function() {
+                var filmStrip = document.getElementById('filmStrip');
+                var busyContext = oj.Context.getContext(filmStrip).getBusyContext();
+                busyContext.whenReady().then(function() {
+                    // Set the Paging Control pagingModel
+                    self.pagingModel(filmStrip.getPagingModel());
+                });
+            };
+
         }
 
         return viewNoticesViewModel;
