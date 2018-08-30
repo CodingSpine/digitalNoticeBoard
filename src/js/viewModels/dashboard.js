09cc8e79e2ad74634476617953dcef5b49dc47aa
Ray Mathew <ray10mathew@gmail.com>
2018-08-31 02:04:27 +0530

    dashboard update

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -1,7 +1,14 @@
-define(['ojs/ojcore', 'knockout', 'globalContext', 'ojs/ojinputtext', 'ojs/ojbutton'],
+define(['ojs/ojcore', 'knockout', 'globalContext', 'ojs/ojlistview', 'ojs/ojinputtext', 'ojs/ojbutton', 'ojs/ojdialog'],
     function (oj, ko, context) {
         function dashboardViewModel() {
             self.noOfReviews = ko.observable(0);
+            //self.imageLocation = ko.observable('../../css/images/1.png');
+
+            self.locations = [
+                '../../css/images/1.png',
+                '../../css/images/2.jpg'
+            ];
+
             self.goToViewNotices = function(){
                 var router = oj.Router.rootInstance;
                 router.go('viewNotices');
@@ -15,6 +22,25 @@ define(['ojs/ojcore', 'knockout', 'globalContext', 'ojs/ojinputtext', 'ojs/ojbut
             self.createNewNotice = function(){
                 var router = oj.Router.rootInstance;
                 router.go('createNewNotice');
+            };
+
+            self.openSlideShow = function(){
+                document.querySelector("#slideshowDialog").open();
+                setTimeout(startSlideShow, 400);
+            };
+
+            var startSlideShow = function(){
+                $("#slideshow > div:gt(0)").hide();
+
+                setInterval(function() {
+                  $('#slideshow > div:first')
+                    .fadeOut(500)
+                    .next()
+                    .fadeIn(500)
+                    .end()
+                    .appendTo('#slideshow');
+                },  5000);
+
             };
         }
 
