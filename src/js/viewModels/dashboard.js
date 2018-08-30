2ee69a9e926b48224bee09c2d4c4dd7a09bc7e12
Ray Mathew <ray10mathew@gmail.com>
2018-08-30 10:58:28 +0530

    started work on dashboard

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -1,9 +1,20 @@
 define(['ojs/ojcore', 'knockout', 'globalContext', 'ojs/ojinputtext', 'ojs/ojbutton'],
     function (oj, ko, context) {
         function dashboardViewModel() {
+            self.noOfReviews = ko.observable(0);
             self.goToViewNotices = function(){
                 var router = oj.Router.rootInstance;
                 router.go('viewNotices');
+            };
+
+            self.reviewNewNotices = function(){
+                var router = oj.Router.rootInstance;
+                router.go('reviewNewNotices');
+            };
+
+            self.createNewNotice = function(){
+                var router = oj.Router.rootInstance;
+                router.go('createNewNotice');
             };
         }
 
