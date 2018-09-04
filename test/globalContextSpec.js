5e8ffa4fe19bd5feed6119da9bf93e178f5a8598
Ray Mathew <ray.mathew@oracle.com>
2018-09-04 17:58:18 +0530

    Karma-Jasmine integrated

---
+++ test/globalContextSpec.js
@@ -0,0 +1,9 @@
+define(['src/js/globalContext'], function(globalContext) {
+debugger;
+    describe('src/js/globalContext.js', function() {
+        it('should initialize the viewModel', function() {
+            expect(globalContext).toBeDefined();
+        });
+    });
+
+});
